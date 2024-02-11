14c22df05e0c213195df1761133d7372322a8941
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2024-02-11 23:31:41 +0530

    Add files via upload

---
+++ p7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,f;
+	printf("Enter two values(integers):");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	printf("Result of addition of %d and %d is %d \n",a,b,c);
+	printf("Result of subtraction  of %d and  %d is %d \n",a,b,d);
+	printf("Result of multiplication of %d and %d is %d \n",a,b,e);
+	printf("Result of division of %d and %d is %d \n",a,b,f);
+	}
+	
+	
