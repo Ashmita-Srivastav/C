eac8c1aed03dfee78d12ebd6a30f8717e69218f6
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2024-02-13 23:31:01 +0530

    Add files via upload

---
+++ p11.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+        float a,b,c;
+        printf("Enter two numbers:");
+        scanf("%f%f",&a,&b);
+        c=a+b;
+        printf("The addition of %f and %f is %f",a,b,c);
+} 
