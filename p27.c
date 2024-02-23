d52e78062a9fb18797d8da9bac27211c994746b1
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2024-02-23 23:49:07 +0530

    Add files via upload

---
+++ p27.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ int a,b,x=20;
+a=x++;
+b=++x;
+printf("\n Value of x is =%d",x);
+printf("\n Value of a is =%d",a);
+printf("\n Value of b is =%d",b);
+printf("\n Value of a is =%d \n",x);
+return 0;
+}
