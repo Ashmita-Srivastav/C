f79555bc55dd71a73da92d6c04c4135b448b3fb8
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2024-03-15 22:53:17 +0530

    Add files via upload

---
+++ p46.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter the number:");
+	scanf("%d",&a);
+	if(a%5==0 && a%11==0)
+	{
+		printf("%d is divisible by 5 and 11",a);
+	}
+	else{
+		printf("%d is not divisible by 5 and 11",a);
+	}
+	return 0;
+}
+	
+	
